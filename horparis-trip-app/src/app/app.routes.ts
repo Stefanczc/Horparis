e9b4ee3c8779d3db3603dfa047aef1044afa1ea3
Alexandru <stefan.alexandru@partner.cgm.com>
2024-11-06 20:33:44 +0200

    home-page-login-navigation

--- horparis-trip-app/src/app/app.routes.ts
+++ horparis-trip-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';  
+import { ItineraryComponent } from './itinerary/itinerary.component'; 
+import { ObjectivesComponent } from './objectives/objectives.component';  
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'itinerary', component: ItineraryComponent },
+  { path: 'objectives', component: ObjectivesComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], 
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- horparis-trip-app/src/app/home/home.component.ts
+++ horparis-trip-app/src/app/home/home.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule], 
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  loginForm: FormGroup;
+
+  constructor(private router: Router) {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),  
+      password: new FormControl('', [Validators.required]) 
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const userData = {
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password,
+      };
+  
+      const localUser = JSON.parse(localStorage.getItem('localUser') || 'null');
+  
+      if (!localUser) {
+        localStorage.setItem('localUser', JSON.stringify(userData));
+        alert('Bine ai venit in Paris!');
+        this.router.navigate(['/itinerary']);
+        return;
+      }
+  
+      if (userData.username === localUser.username && userData.password === localUser.password) {
+        alert('Bine ai venit in Paris!');
+        this.router.navigate(['/itinerary']);
+      } else {
+        alert('Date incorecte!');
+      }
+    } else {
+      alert('Introdu toate datele');
+    }
+  }
+  
 }
