100b9e8a8a68e7472210d12e58cc777dbead4ac2
Alexandru <stefan.alexandru@partner.cgm.com>
2024-11-11 16:40:15 +0200

    new changes

--- horparis-trip-app/src/app/home/home.component.ts
+++ horparis-trip-app/src/app/home/home.component.ts
@@ -11,41 +11,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
-  loginForm: FormGroup;
-
-  constructor(private router: Router) {
-    this.loginForm = new FormGroup({
-      username: new FormControl('', [Validators.required]),  
-      password: new FormControl('', [Validators.required]) 
-    });
-  }
-
-  onLogin() {
-    if (this.loginForm.valid) {
-      const userData = {
-        username: this.loginForm.value.username,
-        password: this.loginForm.value.password,
-      };
-  
-      const localUser = JSON.parse(localStorage.getItem('localUser') || 'null');
-  
-      if (!localUser) {
-        localStorage.setItem('localUser', JSON.stringify(userData));
-        alert('Bine ai venit in Paris!');
-        this.router.navigate(['/itinerary']);
-        return;
-      }
-  
-      if (userData.username === localUser.username && userData.password === localUser.password) {
-        alert('Bine ai venit in Paris!');
-        this.router.navigate(['/itinerary']);
-      } else {
-        alert('Date incorecte!');
-      }
-    } else {
-      alert('Introdu toate datele');
-    }
-  }
   
 }
