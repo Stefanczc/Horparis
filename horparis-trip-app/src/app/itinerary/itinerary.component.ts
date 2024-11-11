e9a14f5dbe269073c1dd833f510734c3754f311b
Alexandru <stefan.alexandru@partner.cgm.com>
2024-11-11 20:37:37 +0200

    new changes

--- horparis-trip-app/src/app/itinerary/itinerary.component.ts
+++ horparis-trip-app/src/app/itinerary/itinerary.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-itinerary',
   standalone: true,
-  imports: [NgFor, CommonModule],
+  imports: [NgFor, CommonModule, FormsModule],
   templateUrl: './itinerary.component.html',
   styleUrl: './itinerary.component.css'
 })
@@ -19,23 +20,27 @@ export class ItineraryComponent {
       objectives: [
       {
         id: 1,
-        name: 'Gara de Nord',
-        picture: '../../assets/day1/gare_du_nord.jfif'
+        name: 'Gare du Nord',
+        picture: '../../assets/day1/gare_du_nord.jfif',
+        checked: false,
       },
       {
         id: 2,
-        name: 'Bazilica Sacré-Cœur',
-        picture: '../../assets/day1/sacre_cour.jfif'
+        name: 'Basilique du Sacré-Cœur',
+        picture: '../../assets/day1/sacre_cour.jfif',
+        checked: false,
       },
       {
         id: 3,
-        name: 'Opera Garnier',
-        picture: '../../assets/day1/palais_garnier.jpg'
+        name: 'Palais Garnier',
+        picture: '../../assets/day1/palais_garnier.jpg',
+        checked: false,
       },
       {
         id: 4,
-        name: 'Muzeul Louvre',
-        picture: '../../assets/day1/louvre_museum.jpg'
+        name: 'Musee du Louvre',
+        picture: '../../assets/day1/louvre_museum.jpg',
+        checked: false,
       }
     ],
       showObjectives: false,
@@ -45,12 +50,82 @@ export class ItineraryComponent {
       id: 2,
       title: 'Activitati',
       description: 'De la osemintele lui Napoleon pana in celebrul Turn Eiffel.',
+      objectives: [
+      {
+        id: 1,
+        name: 'Hotelul des Invalides',
+        picture: '../../assets/day2/hotel_invalides.jpg',
+        checked: false,
+      },
+      {
+        id: 2,
+        name: 'Place de la Concorde',
+        picture: '../../assets/day2/place_concorde.jpg',
+        checked: false,
+      },
+      {
+        id: 3,
+        name: "Musee d'Art Moderne",
+        picture: '../../assets/day2/musee_art.jfif',
+        checked: false,
+      },
+      {
+        id: 4,
+        name: 'Trocadero',
+        picture: '../../assets/day2/trocadero.jfif',
+        checked: false,
+      },
+      {
+        id: 5,
+        name: 'Tour Eiffel',
+        picture: '../../assets/day2/tour-eiffel.jpg',
+        checked: false,
+      }
+    ],
       link: "https://www.google.ro/maps/dir/H%C3%B4tel+des+Invalides,+Paris,+Fran%C5%A3a/Place+de+la+Concorde/''/Mus%C3%A9e+d'Art+Moderne+de+Paris/Place+du+Trocad%C3%A9ro/Tour+Eiffel/@48.8585172,2.2872989,15.71z/data=!3m1!5s0x47e66fcfdcc83aa5:0x4e17f6a5b0e0d0b2!4m43!4m42!1m10!1m1!1s0x47e66fd7b98f3053:0x455a14459c80c16a!2m2!1d2.3127059!2d48.856529!3m4!1m2!1d2.3212023!2d48.8655772!3s0x47e66fd29521f357:0x7ec0e8aa61275e17!1m5!1m1!1s0x47e66fcd61ae0a01:0x18030de10e25ab2c!2m2!1d2.3212357!2d48.8656331!1m5!1m1!1s0x47e66fc50dc75ccf:0x42f0f4fa01438416!2m2!1d2.30997!2d48.86919!1m5!1m1!1s0x47e66fe7988e74af:0x60dec2cd0e194c25!2m2!1d2.2978208!2d48.8643421!1m5!1m1!1s0x47e66fe3405d7ecf:0xc03ac392486d25b5!2m2!1d2.2873248!2d48.8628748!1m5!1m1!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!2m2!1d2.2944813!2d48.8583701!3e2?entry=ttu&g_ep=EgoyMDI0MTAwOS4wIKXMDSoASAFQAw%3D%3D"
     },
     {
       id: 3,
       title: 'Activitati',
       description: "Din Catacombe pana la Catedrala 'Notre-Dame de Paris'.",
+      objectives: [
+        {
+          id: 1,
+          name: 'Catacombs of Paris',
+          picture: '../../assets/day3/catacombes.jfif',
+          checked: false,
+        },
+        {
+          id: 2,
+          name: 'Montparnasse',
+          picture: '../../assets/day3/montparnasse.jpg',
+          checked: false,
+        },
+        {
+          id: 3,
+          name: "Jardin du Luxembourg",
+          picture: '../../assets/day3/luxemburg_jardin.jpg',
+          checked: false,
+        },
+        {
+          id: 4,
+          name: 'Pantheon',
+          picture: '../../assets/day3/pantheon.jpg',
+          checked: false,
+        },
+        {
+          id: 5,
+          name: 'Saint-Chapelle',
+          picture: '../../assets/day3/sainte_chapelle.jpg',
+          checked: false,
+        },
+        {
+          id: 6,
+          name: 'Cathedrale Notre-Dame',
+          picture: '../../assets/day3/notre-damme.jpg',
+          checked: false,
+        }
+      ],
       link: "https://www.google.fr/maps/dir/86+Rue+Raymond+Lefebvre,+94250+Gentilly,+Fran%C5%A3a/Catacombs+of+Paris,+Avenue+du+Colonel+Henri+Rol-Tanguy,+Paris,+Fran%C5%A3a/Montparnasse,+Paris,+Fran%C5%A3a/Jardin+du+Luxembourg,+Paris,+Fran%C5%A3a/Panth%C3%A9on,+Place+du+Panth%C3%A9on,+Paris,+Fran%C5%A3a/Sainte-Chapelle,+Boulevard+du+Palais,+Paris,+Fran%C5%A3a/Cath%C3%A9drale+Notre-Dame+de+Paris,+Parvis+Notre-Dame+-+Place+Jean-Paul-II,+Paris,+Fran%C5%A3a/@48.8378968,2.3321954,13z/data=!4m44!4m43!1m5!1m1!1s0x47e6710b4ff665b1:0x1541088fec314107!2m2!1d2.3400906!2d48.8117068!1m5!1m1!1s0x47e671b6c1d0b675:0xc8d7f073e62eb4b3!2m2!1d2.3323932!2d48.8339119!1m5!1m1!1s0x47e671c7ec66755b:0xc9c5e482069c4490!2m2!1d2.330491!2d48.8393304!1m5!1m1!1s0x47e671db36de687b:0x791dd61b089f98b!2m2!1d2.3363309!2d48.8466144!1m5!1m1!1s0x47e671e7d297c973:0xe5eb004f23a758!2m2!1d2.3464138!2d48.8462218!1m5!1m1!1s0x47e66e1fd8767d47:0x33f441f9dc242768!2m2!1d2.3449609!2d48.855375!1m5!1m1!1s0x47e671e19ff53a01:0x36401da7abfa068d!2m2!1d2.3499021!2d48.8529682!3e2?entry=ttu&g_ep=EgoyMDI0MTEwNi4wIKXMDSoASAFQAw%3D%3D"
     },
     {
